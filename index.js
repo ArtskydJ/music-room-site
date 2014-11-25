aa4b256ed6b561b3af6646452888905758b0836a
Joseph Dykstra <josephdykstra@gmail.com>
2014-11-24 18:25:49 -0600

    start webtorrent and sox

--- index.js
+++ index.js
@@ -1,89 +1,28 @@
 var http = require('http')
-var fs = require('fs')
 var crypto = require('crypto')
 var Ecstatic = require('ecstatic')
-var level = require('level')
-var space = require('level-spaces')
 var Socket = require('socket.io')
-var uploadIsValid = require('./uploadIsValid.js')
+var xtend = require('xtend')
+var websocket = require('websocket')
+//var uploadIsValid = require('./uploadIsValid.js')
+var config = require('./config.js').musicRoom
+var Sux = require('sux')
 
-var serve = Ecstatic({
-	root: __dirname + '/public',
-	showDir: false,
-	gzip: true
-})
+
+config.ecstatic.root = __dirname + config.ecstatic.root
+var serve = Ecstatic(config.ecstatic)
 var server = http.createServer(serve)
 var io = Socket(server)
-var db = level('./database')
 
-var fileDb = space(db, 'file')
-var metaDb = space(db, 'meta')
+
 server.listen(80)
-
 io.on('connection', function (socket) {
 	socket.emit('greeting', 'why, hullo thar')
-	socket.on('upload', upload)
-	socket.on('ls', listUploads)
-	socket.on('del', deleteUploads)
-
-	function upload(file, meta) {
-		if (uploadIsValid(meta)) {
-			var md5 = hash(file)
-			metaDb.put(md5, JSON.stringify(meta), cbIfErr(logErr, function () {
-				fileDb.put(md5, file, cbIfErr(logErr, function () {
-					socket.emit('uploaded', true, meta.name)
-					//console.log('emit: uploaded; ' + meta.name)
-				}))
-			}))
-		} else {
-			socket.emit('uploaded', false, meta.name)
-		}
-	}
-
-	function listUploads() {
-		getFilenames(function (hashnames) {
-			getMetadata(function (hashToMetadata) {
-				var uploadedFilenames = hashnames.map(function (e) {
-					return hashToMetadata[e].name
-				})
-				socket.emit('list uploads', uploadedFilenames)
-				//console.log('emit: list uploads; ' + uploadedFilenames.join(', '))
-			})
-		})
-	}
-
-	function deleteUploads() {
-		deleteAll(fileDb, cbIfErr(logErr, function () {
-			deleteAll(metaDb, cbIfErr(logErr, function () {
-				socket.emit('deleted')
-				//console.log('emit: deleted')
-			}))
-		}))
-	}
 })
 
-function deleteAll(db, cb) {
-	var batch = db.batch()
-	var read = fileDb.createKeyStream()
-	read.on('data', function (key) { batch.del(key) })
-	read.on('end', function () { batch.write(cb) })
-}
-
-function getFilenames(cb) {
-	var filenames = []
-	var read = fileDb.createKeyStream()
-	read.on('data', function (key) { filenames.push(key) })
-	read.on('end', function () { cb(filenames) })
-}
-
-function getMetadata(cb) {
-	var hashMap = {}
-	var read = metaDb.createReadStream()
-	read.on('data', function (pair) {
-		try { hashMap[pair.key] = JSON.parse(pair.value) }
-		catch (err) { logErr(err) }
-	})
-	read.on('end', cb.bind(null, hashMap))
+function convert(inStream) {
+	var convert = new Sux(config.sux)
+	convert.in(inStream).start() //.out()
 }
 
 function cbIfErr(errCb, noErrCb) {
