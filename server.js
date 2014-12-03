f5fa3dfdf34138e0c8e04028b1ccb09257296f7d
Joseph Dykstra <josephdykstra@gmail.com>
2014-12-03 17:19:05 -0600

    more readable if upload is rejected, we are told why socket events are named better also, the server now acts as a torrent client

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http')
 var Ecstatic = require('ecstatic')
 var Socket = require('socket.io')
+var Webtorrent = require('webtorrent')
 var PlaylistCombinator = require('playlist-combinator')
 var config = require('./src/config.json').musicRoom
 
@@ -9,19 +10,51 @@ var serve = Ecstatic(config.ecstatic)
 var server = http.createServer(serve)
 var io = Socket(server)
 var playlist = PlaylistCombinator()
+var torrenter = new Webtorrent()
 
 server.listen(80)
 
 io.on('connection', function (socket) {
 	var userId = socket.id //I don't think these are persistent between shutdowns
+	socket.join('room')
+
 	playlist.addUser(userId)
 
 	socket.emit('greeting', 'why, hullo thar')
-	socket.on('new file', function (infoHash) { //add auth here
+	socket.on('upload', function (infoHash) { //TODO add auth here
 		playlist.addSong(userId, infoHash)
-		socket.broadcast.emit('new file', infoHash)
+		torrenter.download({ //act as a seeder
+			infoHash: infoHash,
+			announce: config.announce
+		})
+		socket.broadcast.emit('download', infoHash) //TODO do this elsewhere
+	})
+	socket.on('disconnect', function () {
+
 	})
 })
 
+/*setInterval(function poll() {
+
+}, 100)*/
+
 
 playlist.on('error', console.log.bind(null, 'playlist error'))
+
+/*
+gets uplaod:
+	add upload to playlist
+
+no songs are playing:
+	there are enough people to play a song:
+		enough people have the song:
+			tell everyone to play the song
+		ELSE:
+			give everyone the song
+
+onsongend:
+	delete just played song (torrent.remove())
+	have all play next song
+
+
+*/
--- src/client.js
+++ src/client.js
@@ -1,25 +1,24 @@
 var Socket = require('socket.io-client')
 var dragDrop = require('drag-drop/buffer')
 var Webtorrent = require('webtorrent')
-var uploadIsValid = require('./uploadIsValid.js')
+var uploadIsInvalid = require('./uploadIsInvalid.js')
 var SongStorage = require('./storage.js')
 var config = require('./config.json').musicRoom
 
 var socket = Socket(config.socketIoUrl)
-var torrent = new Webtorrent()
+var torrenter = new Webtorrent()
 var songStorage = new SongStorage()
 var latestUploadInfoHash = null
 
 socket.on('greeting', function (str) { console.log(str) })
 
-socket.on('new file', function (infoHash) {
+socket.on('download', function (infoHash) {
 	console.log('attempting to download: ' + infoHash)
-				latestUploadInfoHash = infoHash //for window.play()
-	var options = {
+	torrenter.download({
 		infoHash: infoHash,
 		announce: config.announce
-	}
-	torrent.download(options, function onTorrent(torrent) {
+	}, function onTorrent(torrent) {
+		latestUploadInfoHash = infoHash //for window.play()
 		console.log('downloaded ' + infoHash + ' from ' + torrent.swarm.wires.length + ' peers.')
 		songStorage.put(torrent)
 	})
@@ -38,17 +37,22 @@ if (typeof document !== 'undefined') { //if in browser
 	console.log('window.play()')
 
 	dragDrop('#dragDropUpload', function (files, pos) {
-		files.filter(uploadIsValid).forEach(function (file) {
-			torrent.seed(file, function onSeed(torrent) {
-				torrent.discovery.on('peer', function () {
-					console.log('connected to peer')
+		files.forEach(function (file) {
+			var err = uploadIsInvalid(file)
+			if (!err) {
+				torrenter.seed(file, function onSeed(torrent) {
+					torrent.discovery.on('peer', function () {
+						console.log('connected to peer')
+					})
+					var infoHash = torrent.infoHash
+					latestUploadInfoHash = infoHash //for window.play()
+					console.log('created torrent: ' + infoHash)
+					socket.emit('upload', infoHash) //add auth here; emit username too
+					songStorage.put(torrent)
 				})
-				var infoHash = torrent.infoHash
-				latestUploadInfoHash = infoHash //for window.play()
-				console.log('created torrent: ' + infoHash)
-				socket.emit('new file', infoHash) //add auth here; emit username too
-				songStorage.put(torrent)
-			})
+			} else {
+				console.log(err && err.message)
+			}
 		})
 	})
 }
