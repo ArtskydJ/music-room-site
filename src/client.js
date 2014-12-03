275d8242ffa5cd3ce3acf782eb6f14c1796a3537
Joseph Dykstra <josephdykstra@gmail.com>
2014-12-02 22:38:29 -0600

    clean up remove unused dependencies in package.json remove unused dependencies in source code remove ugly comments remove extraneous logging clarify existing logging storage.js exports (none of that ridiculous `songStorage._storage` now)

--- src/client.js
+++ src/client.js
@@ -1,29 +1,26 @@
 var Socket = require('socket.io-client')
 var dragDrop = require('drag-drop/buffer')
 var Webtorrent = require('webtorrent')
-var crypto = require('crypto')
 var uploadIsValid = require('./uploadIsValid.js')
-var reflect = require('./reflect.js')
 var SongStorage = require('./storage.js')
 var config = require('./config.json').musicRoom
-var typedArrayToBuffer = require('typedarray-to-buffer')
-var AV = require('av')
 
 var socket = Socket(config.socketIoUrl)
 var torrent = new Webtorrent()
 var songStorage = new SongStorage()
+var latestUploadInfoHash = null
 
 socket.on('greeting', function (str) { console.log(str) })
 
 socket.on('new file', function (infoHash) {
-	console.log('found new file: ' + infoHash)
+	console.log('attempting to download: ' + infoHash)
+				latestUploadInfoHash = infoHash //for window.play()
 	var options = {
 		infoHash: infoHash,
 		announce: config.announce
 	}
 	torrent.download(options, function onTorrent(torrent) {
 		console.log('downloaded ' + infoHash + ' from ' + torrent.swarm.wires.length + ' peers.')
-		console.log(torrent)
 		songStorage.put(torrent)
 	})
 })
@@ -35,42 +32,23 @@ socket.on('play song', function (playInfoHash, preloadInfoHash) {
 
 if (typeof document !== 'undefined') { //if in browser
 
-	window.play = function play(infoHash) {
-		if (!infoHash) {
-			infoHash = Object.keys(songStorage._storage)[0]
-		}
-		console.log(infoHash)
-		songStorage.get(infoHash).play()
+	window.play = function play() {
+		return songStorage.get( latestUploadInfoHash ).play()
 	}
-	window.get = function get() {return songStorage._storage}
-	console.log('window.play(), window.get()')
+	console.log('window.play()')
 
 	dragDrop('#dragDropUpload', function (files, pos) {
 		files.filter(uploadIsValid).forEach(function (file) {
-
-			/*var player = AV.Player.fromBuffer(file) //THIS WORKS
-			player.play()
-			console.log(player)
-			setTimeout(console.log.bind(console, player), 5000)*/
-
-			torrent.seed(file, function onSeed(torrent) { //{name: hash(file)}, 
+			torrent.seed(file, function onSeed(torrent) {
+				torrent.discovery.on('peer', function () {
+					console.log('connected to peer')
+				})
 				var infoHash = torrent.infoHash
-				if (torrent.discovery && typeof torrent.discovery.on === 'function') {
-					console.log('watching torrent.discovery for peer event')
-					torrent.discovery.on('peer', function () {
-						console.log('connected to peer')
-					})
-				}
+				latestUploadInfoHash = infoHash //for window.play()
 				console.log('created torrent: ' + infoHash)
 				socket.emit('new file', infoHash) //add auth here; emit username too
-				console.log(torrent)
 				songStorage.put(torrent)
-				/*setTimeout(function () {
-					console.log('play attempt')
-					songStorage.get(torrent.infoHash).play()
-				}, 1000)*/
 			})
 		})
 	})
 }
-
--- src/storage.js
+++ src/storage.js
@@ -4,6 +4,10 @@ var concat = require('concat-stream')
 function fail() {
 	console.log('failure')
 }
+var mock = {
+	play: fail,
+	preload: fail
+}
 
 module.exports = function () {
 	var storage = {} //AV.Player instances stored under their `torrent`s `infoHash`s
@@ -13,26 +17,14 @@ module.exports = function () {
 		file.createReadStream().pipe(concat(function (buf) {
 			storage[torrent.infoHash] = AV.Player.fromBuffer(buf)
 		}))
-		
-		// AV.Player.fromBuffer(torrent.files) // returned nothing, and didn't hear audio
-		// new AV.Player(new AV.Asset(file))
-		// new AV.Player(file)                     //this.asset is defined but this.asset.start is undefined and not a function
-		// new AV.Player.fromFile(new File([file], torrent.infoHash)) //somethin doesn't work.
-		// AV.Player.fromBuffer(file)          //throws error about AV being undefined
-		// AV.Player(file.createReadStream())  //returns undefined
-		// AV.Player.fromFile(file)            //returns a Player obj, but throws when .play() is called
 	}
 
 	function get(infoHash) {
-		return storage[infoHash] || {
-			play: fail,
-			preload: fail
-		}
+		return storage[infoHash] || mock
 	}
 
 	return {
 		put: put,
-		get: get,
-		_storage: storage
+		get: get
 	}
 }
