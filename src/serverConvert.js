50360222173ab0a3e320b5016dba299316e006c1
Joseph Dykstra <josephdykstra@gmail.com>
2014-12-19 22:38:55 -0600

    shortened conversion code

--- src/serverConvert.js
+++ src/serverConvert.js
@@ -2,9 +2,7 @@ var config = require('./config.json').musicRoom
 var Sox = require('sox-stream')
 var xtend = require('xtend')
 var createTagData = require('musicmetadata')
-//var filereaderStream = require('filereader-stream')
 var path = require('path')
-//var stream = require('readable-stream')
 
 function getTagData(stream, meta, cb) {
 	var opts = { duration: true, fileSize: meta.size }
@@ -21,39 +19,26 @@ function getTagData(stream, meta, cb) {
 	}
 }
 
-function Convert(newType) {
-	newType = newType.toLowerCase()
+function convert(newType, stream, meta) {
+	var inputType = path.extname(meta.name)
+	var opts = xtend( config.presets[newType], { type: newType })
 
-	return function convert(stream, meta) {
-		var fileName = meta.name
-		var inputType = path.extname(meta.name).toLowerCase()
-
-		if (inputType === newType) {
-			return stream
-		} else {
-			var opts = xtend( config.presets[newType], { type: newType })
-			var sox = new Sox(opts)
-			return stream.pipe(sox)
-		}
-	}
+	return (inputType.toLowerCase() === newType.toLowerCase()) ?
+		stream :
+		stream.pipe(Sox(opts))
 }
 
-module.exports = function MultiConvertor() {
-	var convertToMp3 = Convert('mp3')
-	var convertToOgg = Convert('ogg')
-	//[ '-t', '.mp3', '-', '-c', 2, '-b', 16, '-r', 44100, '-t', 'ogg', '-' ]
+module.exports = function multiConvert(stream, meta, cb) {
+	var mp3Stream = convert('mp3', stream, meta)
+	var oggStream = convert('ogg', stream, meta)
 
-	return function multiConvert(stream, meta, cb) {
-		var mp3Stream = convertToMp3(stream, meta)
-		var oggStream = convertToOgg(stream, meta)
-		getTagData(stream, meta, function end(err, tagData) {
-			if (err && err.message !== 'Could not find metadata header') {
-				cb(err)
-			} else {
-				console.log('mp3Stream', mp3Stream.toString().slice(0, 80))
-				console.log('oggStream', oggStream.toString().slice(0, 80))
-				cb(null, tagData, mp3Stream, oggStream)
-			}
-		})
-	}
+	getTagData(stream, meta, function end(err, tagData) {
+		if (err && err.message !== 'Could not find metadata header') {
+			cb(err)
+		} else {
+			console.log('mp3Stream', mp3Stream.toString().slice(0, 80))
+			console.log('oggStream', oggStream.toString().slice(0, 80))
+			cb(null, tagData, mp3Stream, oggStream)
+		}
+	})
 }
