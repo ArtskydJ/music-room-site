60aea5da9a7434b8ac132c8f9481ff87115328e7
Joseph Dykstra <josephdykstra@gmail.com>
2015-01-24 16:07:39 -0600

    partials are global instead of an instance on each ractive element every thing works...

--- client/views.js
+++ client/views.js
@@ -1,13 +1,12 @@
 var Ractive = require('ractive')
-var partials = require('./partials.js')
+Ractive.partials = require('./partials.js')
 
 module.exports = function Views(data) {
 	return Object.keys(data).reduce(function (views, key) {
 		views[key] = new Ractive({
 			el: '#' + key + 'View',
 			template: '#' + key + 'Template',
-			data: data[key],
-			partials: partials
+			data: data[key]
 		})
 		return views
 	}, {})
