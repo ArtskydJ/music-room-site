d0d10061f95eb52f4b059fdbf23a311ed0a0fa82
Joseph Dykstra <josephdykstra@gmail.com>
2015-01-29 13:11:17 -0600

    rename stuff

--- client/room/room.js
+++ client/room/room.js
@@ -3,13 +3,14 @@ var resolve = require('../../resolve-namespace.js')
 var EventEmitter = require('events').EventEmitter
 
 module.exports = function (namespace) {
-	var io = Client(resolve(namespace))
+	return Client( resolve(namespace) )
+	/*var io = Client( resolve(namespace) )
 	var em = new EventEmitter
 
-	io.on('receive', em.emit.bind(em, 'receive'))
-	em.on('send',    io.emit.bind(io, 'send'))
+	io.on('chat receive', em.emit.bind(em, 'receive'))
+	em.on('send',         io.emit.bind(io, 'chat send'))
 
 	io.on('new song', em.emit.bind(em, 'new song'))
 	
-	return em
+	return em*/
 }
