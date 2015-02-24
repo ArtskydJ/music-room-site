230ad4da51e276097ee1057eee01da05d26fcb47
Joseph Dykstra <josephdykstra@gmail.com>
2015-02-23 21:49:58 -0600

    rename test to match

--- test/test-socket-manager.js
+++ test/test-socket-manager.js
@@ -1,53 +1,64 @@
 var test = require('tape')
 var EventEmitter = require('events').EventEmitter
-var SessionManager = require('../server/session-manager.js')
+var SocketManager = require('../server/socket-manager.js')
 var connectSession = require('../client/connect-session.js')
 var Level = require('level-mem')
 var StateHolder = require('state-holder')
 var bypass = require('just-login-bypass')
 var Promise = require('promise')
 
-function makeIo() {
-	var rooms = {}
-	var io = new EventEmitter()
-	io.in = function ioin(room) {
-		if (!rooms[room]) {
-			rooms[room] = new EventEmitter()
-		}
-		return rooms[room]
+function expect(t, x, msg) {
+	return function (y) {
+		t.equal(x, y, msg)
 	}
-	return io
 }
 
-function makeSocket(io) {
-	var inRooms = []
+function id() {
+	return Math.random().toString().slice(2)
+}
+
+function makeSession(t) {
+	// I don't think this makes much sense...
+	// Try getting 8 hours of sleep, and then figuring it out.
+	var fullApi = {
+		isAuthenticated: expect(t, 0, 'is auth'),
+		beginAuthentication: expect(t, 1, 'begin auth'),
+		unauthenticate: expect(t, 2, 'unauth')
+	}
+	var sid = id()
+	return function (cb1, cb2) {
+		var cb = cb2 || cb1
+		process.nextTick(function () {
+			cb(null, fullApi, sid)
+		})
+	}
+}
+
+function makeSocket() {
 	var socket = new EventEmitter()
-	socket.id = Math.random().toString().slice(2)
+	socket.id = id()
 	socket.join = function join(room) {
 		socket.emit('_join', room)
 	}
 	socket.leave = function leave(room) {
 		socket.emit('_leave', room)
 	}
-	io.emit('connect', socket)
 	return socket
 }
 
-function makeSessionManager() {
-	var io = makeIo()
-	var core = SessionManager(io, new Level())
-	if (false) {
-		// delete the if block when the just-login-emailer
-		// is implemented in session-manager.js
-		io.removeAllListeners('authentication initiated')
-		bypass(core)
+function makeSessionManager(t) {
+	var db = new Level()
+	var manager = {
+		createSession: makeSession(t),
+		continueSession: makeSession(t)
 	}
-	var socket = makeSocket(io)
+	var socket = makeSocket()
+	SocketManager(db, manager)(socket)
 	return socket
 }
 
-function establishSession() {
-	var socket = makeSessionManager()
+function establishSession(t) {
+	var socket = makeSessionManager(t)
 	var state = StateHolder()
 	return connectSession(socket, state).then(function (sessionId) {
 		return Promise.resolve(socket)
@@ -85,7 +96,7 @@ test('client/connect-session.js', function (t) {
 test('client/connect-session.js', function (t) {
 	t.plan(3)
 
-	establishSession()
+	establishSession(t)
 	.then(function (socket) {
 		var socketEmit = Promise.denodeify( socket.emit.bind(socket) )
 
